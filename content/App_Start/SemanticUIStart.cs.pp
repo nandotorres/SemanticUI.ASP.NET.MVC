07e89943817ed00f49cb7691984d32162d83fa80
nandotorres <nandotorres@gmail.com>
2014-07-29 17:09:24 -0300

    Implement WebActivator and Layout

---
+++ content/App_Start/SemanticUIStart.cs.pp
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(
+      typeof($rootnamespace$.SemanticUIStart), "PostStart")]
+
+
+namespace $rootnamespace$
+{
+    public static class SemanticUIStart
+    {
+        public static void PostStart()
+        {
+            BundleTable.Bundles.Add(new ScriptBundle("~/bundles/semantic").
+			   Include("~/Scripts/semantic.js",
+			           "~/Scripts/semantic.site.js"));
+			   
+            BundleTable.Bundles.Add(new StyleBundle("~/Content/semantic").
+			   Include("~/Content/semantic.css",
+			           "~/Content/semantic.site.css"));
+        }
+    }
+}
